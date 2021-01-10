0047c26b59c51398df1ec8dd6e5a6d33a67ea315
xrawvelocity <vmfs99@gmail.com>
2021-01-10 13:20:14 -0500

    no unused vars...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component, Fragment } from "react";
 import Navigation from "./components/Navigation";
 import Home from "./components/Home";
-import Services from "./components/Services";
+// import Services from "./components/Services";
 import About from "./components/About";
-import Reviews from "./components/Reviews";
+// import Reviews from "./components/Reviews";
 import Contact from "./components/Contact";
 import FAQ from "./components/Faq";
 import Footer from "./components/Footer";
