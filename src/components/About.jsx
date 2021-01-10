3a52ffe7434fa1c5b185d3708a72aa686b6a9f91
xrawvelocity <vmfs99@gmail.com>
2021-01-10 14:30:03 -0500

    .

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ export default class About extends Component {
                     <div className="about-content-image"></div>
 
                     <p className="about-content-text">
-                        My dream is to deliver the word of God to those who need
+                        My name is Luis Valentin and my dream is to deliver the word of God to those who need
                         it, to spread love, and to build faith. I have been a
                         pastor for over 6 years now and I feel more confident
                         than ever when I'm preaching. To schedule an appointment
